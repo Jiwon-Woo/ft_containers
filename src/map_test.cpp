57991e29711657820fc3fadb060f20ab0287341b
Jiwon-Woo <jwoo@student.42seoul.kr>
2022-07-25 10:52:50 +0900

    REFACT: 개인 map 테스트 코드 수정

--- src/map_test.cpp
+++ src/map_test.cpp
@@ -7,20 +7,22 @@ int main()
 	std::map<int, int> m;
 	std::map<int, int>::iterator mi;
 
+	std::cout << "erase empty map: " << m.erase(0) << std::endl;
+
 	m[1] = 3;
 	m[2] = 6;
 	m[0] = 0;
 
+	std::cout << "erase invalid key: " << m.erase(-1) << std::endl;
+	std::cout << "erase key: " << m.erase(0) << std::endl;
+
+
 	// std::cout << (m.begin() == m.end()) << std::endl;
 
 	for (mi = m.begin(); mi != m.end(); mi++) {
-		std::cout << mi->first << std::endl;
-		std::cout << mi->second << std::endl;
+		std::cout << mi->first << ": " << mi->second << std::endl;
 	}
-	std::cout << mi->first << std::endl;
-	std::cout << mi->second << std::endl;
-	++mi;
-	std::cout << mi->first << std::endl;
-	std::cout << mi->second << std::endl;
+	std::cout << "[end]" << std::endl;
+	std::cout << mi->first << ": " << mi->second << std::endl;
 	return 0;
 }
